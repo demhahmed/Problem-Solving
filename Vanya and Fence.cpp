e0cf16463f875c04bb1088b48de32e2964aaa67c
Ahmed Maher <demha.ahmed@yahoo.com>
2021-03-29 10:20:55 +0200

    Update Vanya and Fence.cpp

--- Vanya and Fence.cpp
+++ Vanya and Fence.cpp
@@ -1,1 +1,27 @@
 // http://codeforces.com/contest/677/problem/A
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double n, h;
+  cin >> n >> h;
+
+  int width = 0;
+  int height;
+
+  for (int i = 0; i < n; i++) {
+    cin >> height;
+    if (height > h) {
+      width = width + 2;
+    }
+    else {
+      width++;
+    }
+  }
+
+  cout << width << endl;
+
+  return 0;
+}
